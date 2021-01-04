9de1afdbcde481036e4e5f0be83e0c4f19513709
goshinh <jiller@126.com>
2021-01-04 08:40:01 +0800

    Site updated: 2021-01-04 08:39:46

--- source/_posts/LearningCPlusPlusWithKids/printSmile.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-  cout<<"Smile!Smile!Smile!"<<endl;
-  cout<<"Smile!Smile!"<<endl;
-  cout<<"Smile!"<<endl;
-  return 0;
-}
--- source/_posts/LearningCPlusPlusWithKids/printYourNameAndClass.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-  cout<<"我是张三，我在四（2）班。"
-  return 0;
-}
