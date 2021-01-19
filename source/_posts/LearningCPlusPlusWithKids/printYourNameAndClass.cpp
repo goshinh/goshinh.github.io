39f6af3284f45cc6b81c2bf54258f40976530367
goshinh <jiller@126.com>
2021-01-19 12:40:44 +0800

    Site updated: 2021-01-19 12:40:38

---
+++ source/_posts/LearningCPlusPlusWithKids/printYourNameAndClass.cpp
@@ -0,0 +1,23 @@
+---
+title: printYourNameAndClass
+date: 2020-12-25
+img: 
+top: false
+categories:
+- 和孩子们你一起学习C++
+
+tag: 
+- 编程
+- C++
+- 参考答案
+
+---
+```cpp
+#include<iostream>
+using namespace std;
+int main()
+{
+  cout<<"我是张三，我在四（2）班。"
+  return 0;
+}
+```
